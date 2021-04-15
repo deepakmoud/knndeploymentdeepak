dcd716f8c88fe244fcf186f68934f24d76aea0ce
deepakmoud <deepakmoud@poornima.org>
2021-04-15 14:23:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import pandas as pd
 st.set_option('deprecation.showfileUploaderEncoding', False)
 # Load the pickled model
 model = pickle.load(open('knnmodel.pkl', 'rb'))
-dataset= pd.read_csv('Social_Network_Ads (1).csv')
+dataset= pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, [2, 3]].values
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
